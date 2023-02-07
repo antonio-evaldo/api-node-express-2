ef35d8885c5233e4c180411c2199f15aa8ed2283
Antônio Evaldo <antonio.evaldo349@gmail.com>
2023-02-07 16:17:15 -0300

    video-2.3

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -1,17 +1,16 @@
 import mongoose from "mongoose";
+import ErroBase from "../erros/ErroBase.js";
+import ErroValidacao from "../erros/ErroValidacao.js";
+import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
 
 // eslint-disable-next-line no-unused-vars
 function manipuladorDeErros (erro, req, res, next) {
   if (erro instanceof mongoose.Error.CastError) {
-    res.status(400).send({message: "Um ou mais dados fornecidos estão incorretos."});
+    new RequisicaoIncorreta().enviarResposta(res);
   } else if (erro instanceof mongoose.Error.ValidationError) {
-    const mensagensErro = Object.values(erro.errors)
-      .map(erro => erro.message)
-      .join("; ");
-
-    res.status(400).send({message: `Os seguintes erros foram encontrados: ${mensagensErro}`});
+    new ErroValidacao(erro).enviarResposta(res);
   } else {
-    res.status(500).send({message: "Erro interno de servidor."});
+    new ErroBase().enviarResposta(res);
   }
 }
 
