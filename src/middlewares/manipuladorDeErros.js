84ea15daaef235dfc7c2e438c3dcd29c78b4fe22
Antônio Evaldo <antonio.evaldo349@gmail.com>
2023-02-07 15:27:43 -0300

    video-2.2

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -4,6 +4,12 @@ import mongoose from "mongoose";
 function manipuladorDeErros (erro, req, res, next) {
   if (erro instanceof mongoose.Error.CastError) {
     res.status(400).send({message: "Um ou mais dados fornecidos estão incorretos."});
+  } else if (erro instanceof mongoose.Error.ValidationError) {
+    const mensagensErro = Object.values(erro.errors)
+      .map(erro => erro.message)
+      .join("; ");
+
+    res.status(400).send({message: `Os seguintes erros foram encontrados: ${mensagensErro}`});
   } else {
     res.status(500).send({message: "Erro interno de servidor."});
   }
