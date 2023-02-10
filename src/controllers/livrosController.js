8f13fba150306ec6ac7b938735068757ba5e5aef
Antônio Evaldo <antonio.evaldo349@gmail.com>
2023-02-10 13:49:36 -0300

    video-5.1

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,15 +1,29 @@
 import NaoEncontrado from "../erros/NaoEncontrado.js";
+import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
 import { autores, livros } from "../models/index.js";
 
 class LivroController {
 
   static listarLivros = async (req, res, next) => {
     try {
-      const livrosResultado = await livros.find()
-        .populate("autor")
-        .exec();
+      let { limite = 5, pagina = 1 } = req.query;
 
-      res.status(200).json(livrosResultado);
+      limite = parseInt(limite);
+      pagina = parseInt(pagina);
+
+      if (limite > 0 && pagina > 0) {
+        const livrosResultado = await livros.find()
+          .skip((pagina - 1) * limite)
+          .limit(limite)
+          .populate("autor")
+          .exec();
+
+        res.status(200).json(livrosResultado);
+      } else {
+        next(new RequisicaoIncorreta());
+      }
+
+
     } catch (erro) {
       next(erro);
     }
--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 import ErroBase from "../erros/ErroBase.js";
 import ErroValidacao from "../erros/ErroValidacao.js";
-import NaoEncontrado from "../erros/NaoEncontrado.js";
 import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
 
 // eslint-disable-next-line no-unused-vars
@@ -10,7 +9,7 @@ function manipuladorDeErros (erro, req, res, next) {
     new RequisicaoIncorreta().enviarResposta(res);
   } else if (erro instanceof mongoose.Error.ValidationError) {
     new ErroValidacao(erro).enviarResposta(res);
-  } else if (erro instanceof NaoEncontrado) {
+  } else if (erro instanceof ErroBase) {
     erro.enviarResposta(res);
   } else {
     new ErroBase().enviarResposta(res);
