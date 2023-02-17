e4751a49e413672dc9417be1cf4c2af42daa0c45
Antônio Evaldo <antonio.evaldo349@gmail.com>
2023-02-17 16:01:13 -0300

    video-1.2

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
-mongoose.connect("mongodb+srv://alura:123@alura.dkjed.mongodb.net/alura-node");
+mongoose.connect(process.env.STRING_CONEXAO_DB);
 
 let db = mongoose.connection;
 
