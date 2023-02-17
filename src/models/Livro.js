d2f41527b730b0751f8c39720123787678d5f35e
Antônio Evaldo <antonio.evaldo349@gmail.com>
2023-02-17 16:12:34 -0300

    video-3.2

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -22,8 +22,12 @@ const livroSchema = new mongoose.Schema(
     },
     numeroPaginas: {
       type: Number,
-      min: [10, "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"],
-      max: [5000, "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"]
+      validate: {
+        validator: (valor) => {
+          return valor >= 10 && valor <= 5000;
+        },
+        message: "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"
+      }
     }
   }
 );
